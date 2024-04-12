cc71eb1976a1bd938fbca44dddf3ea4cbe55a5cd
HridaySarder <hridoyaug12@gmail.com>
2024-04-12 10:55:35 +0600

    added the correct ans

--- divisor_dividend.c
+++ divisor_dividend.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int d, q, r, div;
+    int d, q, r, x, div;
     scanf("%d %d %d", &d, &q, &r);
-    if (d <= 4000 && q < 100 && r < 20)
-    {
-        q = d / div;
-        r = d % div;
-        printf("%d", div);
-    }
+    x = d - r;
+    div = x / q;
+    printf("%d\n", div);
     return 0;
 }
